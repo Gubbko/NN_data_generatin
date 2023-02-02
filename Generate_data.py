b183265873ec29b3572419e14f227ebbe06434c3
Gubbko <Vladimir.Volotsajev@gmail.com>
2023-02-02 10:46:37 +0200

    Created class to generate image

--- Generate_data.py
+++ Generate_data.py
@@ -1,96 +1,79 @@
 import Generate_image
-from cv2 import VideoCapture, imread, resize, imwrite
+from cv2 import VideoCapture, imread, resize, imwrite, rectangle
 
-Video_Path = "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-39-53.mp4"
+Video_Path = [
+    "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-39-53.mp4",
+    "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-42-08.mp4",
+    "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-47-28.mp4",
+    "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-49-42.mp4",
+    "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-13-00-00.mp4",
+    ]
 
 Path_to_save_images = "C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\images\\new\\"
-img_for_training = imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\Stop.jpg")
 
+img_for_training = [
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Circle_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Circle_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Circle_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\CrossWalk_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\CrossWalk_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\CrossWalk_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Forward_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Forward_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Forward_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Highway_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Highway_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Highway_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\MainRoad_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\MainRoad_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\MainRoad_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoHighway_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoHighway_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoHighway_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoWay_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoWay_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\NoWay_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Parking_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Parking_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Parking_3.jpg"),
+    
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Stop_1.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Stop_2.jpg"),
+    imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\all_signs\\Stop_3.jpg")
+    ]
 
-# This value reduce frames that are used to generate image
 every_frame = 3
+
 # here you can cut collor of background
-#upperbounds_for_chromokey = [220, 220, 220]
-#lowerbounds_for_chromokey = [0, 0, 0]
+upperbounds_for_chromokey = [220, 220, 220]
+lowerbounds_for_chromokey = [0, 0, 0]
+bounds = [upperbounds_for_chromokey, lowerbounds_for_chromokey]
 
-# coordinates to put image for training
-place = [100, 100]
+cap = Generate_image.images_by_video(Video_Path[0], every_frame)
+frame = cap.get_frame()
+sign = cap.resize_training_image(img_for_training[0], 60, 120)
+x, y = cap.rand_chords(frame, sign)
+cap.add_sign_without_background_to_image(frame, sign, x, y, bounds)
 
-# new size of image for training
-#img_size = (120, 120)
-
-sign_size = (120, 120)
-
-cap1 = VideoCapture(Video_Path)
 count = 0
 count_1 = 0
-"""
-while 1:
-    frame1 = cap1.read()[1]
-    frame2 = cap1.read()[1]
-    frame3 = cap1.read()[1]
-    sign1 = resize(img_for_training, sign_size1)
-    sign2 = resize(img_for_training, sign_size2)
-    sign3 = resize(img_for_training, sign_size3)
-    
-    if count % every_frame == 0:
-        x1, y1 = Generate_image.rand_chords(frame1, sign1)
-        x2, y2 = Generate_image.rand_chords(frame2, sign2)
-        x3, y3 = Generate_image.rand_chords(frame3, sign3)
-        
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1),
-                Generate_image.add_sign_without_background_to_image(frame1,
-                                                                    sign1,
-                                                                    x1, y1,
-                                                                    lowerbounds_for_chromokey,
-                                                                    upperbounds_for_chromokey,
-                                                                    place))
-        
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+2),
-                Generate_image.add_sign_without_background_to_image(frame2,
-                                                                    sign2,
-                                                                    x2 , y2,
-                                                                    lowerbounds_for_chromokey,
-                                                                    upperbounds_for_chromokey,
-                                                                    place))
-        
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+3),
-                Generate_image.add_sign_without_background_to_image(frame3,
-                                                                    sign3,
-                                                                    x3, y3,
-                                                                    lowerbounds_for_chromokey,
-                                                                    upperbounds_for_chromokey,
-                                                                    place))
-        
-        count_1 = count_1 + 3
-    count = count + 1
-"""
-
-
-# upperbounds_for_chromokey, lowerbounds_for_chromokey, img_size
-to_generate_image = Generate_image.image_processing([220, 220, 220], [0, 0, 0], (120, 120))
-
 
 while 1:
-    frame = cap1.read()[1]
-    sign = resize(img_for_training, sign_size)
-    
-    #print(len(frame))
-    to_generate_image.get_random_coordinate_to_put_image(frame, sign)
-    #x, y = to_generate_image.get_random_coordinate_to_put_image(frame, sign)
-    #frame_to = to_generate_image.add_sign_without_background_to_image(frame, sign, x = 0, y = 0, place = 100)
-    
-    """
-    if count % every_frame == 0:
-        x1, y1 = Generate_image.rand_chords(frame, sign)
-        
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1),
-                Generate_image.add_sign_without_background_to_image(frame_to,
-                                                                    sign,
-                                                                    x1, y1,
-                                                                    place))
-        
-        count_1 = count_1 + 3
-    count = count + 1
-    """
-    
+    try:
+        frame = cap.get_frame()
+        if count % every_frame == 0:
+            sign = cap.resize_training_image(img_for_training[0], 60, 120)
+            x, y = cap.rand_chords(frame, sign)
+            imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1), cap.add_sign_without_background_to_image(frame, sign, x, y, bounds))
+            count_1 = count_1 + 3
+        count = count + 1
+    except KeyboardInterrupt:
+        print("Interupt")
+        break
--- Generate_image.py
+++ Generate_image.py
@@ -1,98 +1,52 @@
-#from cv2 import VideoCapture, imread, resize, imwrite
+# sourcery skip: hoist-statement-from-loop, merge-nested-ifs
+from cv2 import VideoCapture, imread, resize, imwrite, rectangle
 import random
+# https://docs.opencv.org/3.4/d9/d61/tutorial_py_morphological_ops.html
 
-def rand_chords(img, sign):
+class images_by_video():
+    def __init__(self, video, frames_iter = 10):
+        self.cap = VideoCapture(video)
+        self.frames_iter = frames_iter
+
+    def get_frame(self):
+        return self.cap.read()[1]
     
-    base_x = len(img)
-    subs_x = len(sign)
-    base_y = len(img[0])
-    subs_y = len(sign[0])
-
-    max_x = base_x // 2 - subs_x // 4
-    max_y = base_y // 2 - subs_y // 2
-    
-    new_x_chord = random.randint(0, max_x)
-    new_y_chord = random.randint(subs_y // 2,max_y)
-
-    return -new_x_chord, -new_y_chord
-    
-def add_sign_without_background_to_image(frame_to, sign, x = 0, y = 0, lowerbounds_for_chromokey=None, upperbounds_for_chromokey=None, place = 100):
-    if lowerbounds_for_chromokey is None:
-        lowerbounds_for_chromokey = [0, 0, 0]
-    if upperbounds_for_chromokey is None:
-        upperbounds_for_chromokey = [220, 220, 220]
-    for i in range(len(sign)):
-        for j in range(len(sign[i])):
-            if (
-                lowerbounds_for_chromokey[0]
-                < sign[i][j][0]
-                < upperbounds_for_chromokey[0]
-                and lowerbounds_for_chromokey[1]
-                < sign[i][j][1]
-                < upperbounds_for_chromokey[1]
-                and lowerbounds_for_chromokey[2]
-                < sign[i][j][2]
-                < upperbounds_for_chromokey[2]
-            ):
-                frame_to[i+place[0] + x][j+place[1] + y] = sign[i][j]
-    return frame_to
-
-
-#---------------------------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------------------
-class image_processing():
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    def __init__(self,
-                 upperbounds_for_chromokey=None,
-                 lowerbounds_for_chromokey=None,
-                 every_frame = None
-                 ):
-        
-        if upperbounds_for_chromokey    is None: upperbounds_for_chromokey  = [220, 220, 220]
-        if lowerbounds_for_chromokey    is None: lowerbounds_for_chromokey  = [0, 0, 0]
-        if every_frame                  is None: every_frame                = (120, 120)
-            
-        self.upperbounds_for_chromokey = upperbounds_for_chromokey
-        self.lowerbounds_for_chromokey = lowerbounds_for_chromokey
-        self.every_frame = every_frame
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    def get_random_coordinate_to_put_image(self, img, sign):
-        #print(len(img))
-        #print(type(len(img)))
+    def rand_chords(self, img, sign):
         base_x = len(img)
-        #subs_x = len(sign)
-        #base_y = len(img[0])
-        #subs_y = len(sign[0])
-        
-        #max_x = base_x // 2 - subs_x // 4
-        #max_y = base_y // 2 - subs_y // 2
-        
-        #new_x_chord = random.randint(0, max_x)
-        #new_y_chord = random.randint(subs_y // 2,max_y)
-        
-        #return -new_x_chord, -new_y_chord
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    def add_sign_without_background_to_image(self, frame_to, sign, x = 0, y = 0, place = 100):
+        subs_x = len(sign)
+        base_y = len(img[0])
+        subs_y = len(sign[0])
+        #print(f"base = {base_x}:{base_y} -------------- subs = {subs_x}:{subs_y}")
+        max_x = base_x - subs_x
+        max_y = base_y - subs_y
+        new_x_chord = random.randint(0, max_x)
+        new_y_chord = random.randint(0, max_y)
+        return new_x_chord, new_y_chord
+    
+    def add_sign_without_background_to_image(self, frame_to, sign, x, y, bounds):
+        lowerbounds_for_chromokey = bounds[1]
+        upperbounds_for_chromokey = bounds[0]
         for i in range(len(sign)):
             for j in range(len(sign[i])):
                 if (
-                    self.lowerbounds_for_chromokey[0]
+                    lowerbounds_for_chromokey[0]
                     < sign[i][j][0]
-                    < self.upperbounds_for_chromokey[0]
-                    and self.lowerbounds_for_chromokey[1]
+                    < upperbounds_for_chromokey[0]
+                    and lowerbounds_for_chromokey[1]
                     < sign[i][j][1]
-                    < self.upperbounds_for_chromokey[1]
-                    and self.lowerbounds_for_chromokey[2]
+                    < upperbounds_for_chromokey[1]
+                    and lowerbounds_for_chromokey[2]
                     < sign[i][j][2]
-                    < self.upperbounds_for_chromokey[2]
+                    < upperbounds_for_chromokey[2]
                 ):
-                    frame_to[i+place[0] + x][j+place[1] + y] = sign[i][j]
+                    frame_to[i+x][j+y] = sign[i][j]
+        #rectangle(frame_to,(y,x),(y+len(sign[0]),x+len(sign)),(0,255,0),2)
         return frame_to
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#---------------------------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------------------
+    
+    def resize_training_image(self, sign, min = 60, max = 120):
+        size = random.randint(min, max)
+        sign = resize(sign, [size, size])
+        return sign
 
-if __name__ == '__main__':
-    print("Can not be used separatly")
+if __name__ == "__main__":
+    print("main")
