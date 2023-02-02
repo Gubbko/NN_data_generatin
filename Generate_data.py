0e174ed678e76e4fd9daa19e3fdc872c281b9934
Gubbko <Vladimir.Volotsajev@gmail.com>
2023-02-02 09:11:33 +0200

    1

--- Generate_data.py
+++ Generate_data.py
@@ -6,26 +6,25 @@ Video_Path = "C:\\Users\\Gubbko\\Desktop\\Bosch\\Videos\\" + "21-09-17-12-39-53.
 Path_to_save_images = "C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\images\\new\\"
 img_for_training = imread("C:\\Users\\Gubbko\\Desktop\\Bosch\\Lane_2023\\Stop.jpg")
 
-every_frame = 3
 
+# This value reduce frames that are used to generate image
+every_frame = 3
 # here you can cut collor of background
-upperbounds_for_chromokey = [220, 220, 220]
-lowerbounds_for_chromokey = [0, 0, 0]
+#upperbounds_for_chromokey = [220, 220, 220]
+#lowerbounds_for_chromokey = [0, 0, 0]
 
 # coordinates to put image for training
 place = [100, 100]
 
 # new size of image for training
-img_size = (120, 120)
+#img_size = (120, 120)
 
-sign_size1 = (120, 120)
-sign_size2 = (100, 100)
-sign_size3 = (80, 80)
+sign_size = (120, 120)
 
 cap1 = VideoCapture(Video_Path)
 count = 0
 count_1 = 0
-
+"""
 while 1:
     frame1 = cap1.read()[1]
     frame2 = cap1.read()[1]
@@ -34,13 +33,10 @@ while 1:
     sign2 = resize(img_for_training, sign_size2)
     sign3 = resize(img_for_training, sign_size3)
     
-    
-
     if count % every_frame == 0:
         x1, y1 = Generate_image.rand_chords(frame1, sign1)
         x2, y2 = Generate_image.rand_chords(frame2, sign2)
         x3, y3 = Generate_image.rand_chords(frame3, sign3)
-        
         
         imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1),
                 Generate_image.add_sign_without_background_to_image(frame1,
@@ -68,4 +64,33 @@ while 1:
         
         count_1 = count_1 + 3
     count = count + 1
+"""
+
+
+# upperbounds_for_chromokey, lowerbounds_for_chromokey, img_size
+to_generate_image = Generate_image.image_processing([220, 220, 220], [0, 0, 0], (120, 120))
+
+
+while 1:
+    frame = cap1.read()[1]
+    sign = resize(img_for_training, sign_size)
+    
+    #print(len(frame))
+    to_generate_image.get_random_coordinate_to_put_image(frame, sign)
+    #x, y = to_generate_image.get_random_coordinate_to_put_image(frame, sign)
+    #frame_to = to_generate_image.add_sign_without_background_to_image(frame, sign, x = 0, y = 0, place = 100)
+    
+    """
+    if count % every_frame == 0:
+        x1, y1 = Generate_image.rand_chords(frame, sign)
+        
+        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1),
+                Generate_image.add_sign_without_background_to_image(frame_to,
+                                                                    sign,
+                                                                    x1, y1,
+                                                                    place))
+        
+        count_1 = count_1 + 3
+    count = count + 1
+    """
     
--- Generate_image.py
+++ Generate_image.py
@@ -38,10 +38,61 @@ def add_sign_without_background_to_image(frame_to, sign, x = 0, y = 0, lowerboun
     return frame_to
 
 
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
 class image_processing():
-    def __init__(self):
-        pass
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    def __init__(self,
+                 upperbounds_for_chromokey=None,
+                 lowerbounds_for_chromokey=None,
+                 every_frame = None
+                 ):
+        
+        if upperbounds_for_chromokey    is None: upperbounds_for_chromokey  = [220, 220, 220]
+        if lowerbounds_for_chromokey    is None: lowerbounds_for_chromokey  = [0, 0, 0]
+        if every_frame                  is None: every_frame                = (120, 120)
+            
+        self.upperbounds_for_chromokey = upperbounds_for_chromokey
+        self.lowerbounds_for_chromokey = lowerbounds_for_chromokey
+        self.every_frame = every_frame
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    def get_random_coordinate_to_put_image(self, img, sign):
+        #print(len(img))
+        #print(type(len(img)))
+        base_x = len(img)
+        #subs_x = len(sign)
+        #base_y = len(img[0])
+        #subs_y = len(sign[0])
+        
+        #max_x = base_x // 2 - subs_x // 4
+        #max_y = base_y // 2 - subs_y // 2
+        
+        #new_x_chord = random.randint(0, max_x)
+        #new_y_chord = random.randint(subs_y // 2,max_y)
+        
+        #return -new_x_chord, -new_y_chord
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    def add_sign_without_background_to_image(self, frame_to, sign, x = 0, y = 0, place = 100):
+        for i in range(len(sign)):
+            for j in range(len(sign[i])):
+                if (
+                    self.lowerbounds_for_chromokey[0]
+                    < sign[i][j][0]
+                    < self.upperbounds_for_chromokey[0]
+                    and self.lowerbounds_for_chromokey[1]
+                    < sign[i][j][1]
+                    < self.upperbounds_for_chromokey[1]
+                    and self.lowerbounds_for_chromokey[2]
+                    < sign[i][j][2]
+                    < self.upperbounds_for_chromokey[2]
+                ):
+                    frame_to[i+place[0] + x][j+place[1] + y] = sign[i][j]
+        return frame_to
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
 
-
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    print("Can not be used separatly")
