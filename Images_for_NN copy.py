c4bb80640e3b9c137e96ba1ffb7f4312c1c40a95
Gubbko <Vladimir.Volotsajev@gmail.com>
2023-02-02 10:07:52 +0200

    Fixed error with wrong placement of sign on frame

--- Images_for_NN copy.py
+++ Images_for_NN copy.py
@@ -1,5 +1,5 @@
 # sourcery skip: hoist-statement-from-loop, merge-nested-ifs
-from cv2 import VideoCapture, imread, resize, imwrite
+from cv2 import VideoCapture, imread, resize, imwrite, rectangle
 import random
 # https://docs.opencv.org/3.4/d9/d61/tutorial_py_morphological_ops.html
 
@@ -77,22 +77,23 @@ def rand_chords(img, sign):
     subs_x = len(sign)
     base_y = len(img[0])
     subs_y = len(sign[0])
+    
+    #print(f"base = {base_x}:{base_y} -------------- subs = {subs_x}:{subs_y}")
 
-    max_x = base_x // 2 - subs_x // 4
-    max_y = base_y // 2 - subs_y // 2
+    max_x = base_x - subs_x
+    max_y = base_y - subs_y
 
     #print(f"base_x={base_x},sub={subs_x}, max={max_x}")
     #print(f"base_x={base_y},sub={subs_y}, max={max_y}")
 
     new_x_chord = random.randint(0, max_x)
-    new_y_chord = random.randint(subs_y // 2,max_y)
+    new_y_chord = random.randint(0, max_y)
 
     #print(new_x_chord)
     #print(max_x)
-
-    return -new_x_chord, -new_y_chord
+    return new_x_chord, new_y_chord
     
-def add_sign_without_background_to_image(frame_to, sign, x = 0, y = 0):
+def add_sign_without_background_to_image(frame_to, sign, x, y):
     for i in range(len(sign)):
         for j in range(len(sign[i])):
             if (
@@ -106,41 +107,48 @@ def add_sign_without_background_to_image(frame_to, sign, x = 0, y = 0):
                 < sign[i][j][2]
                 < upperbounds_for_chromokey[2]
             ):
-                frame_to[i+place[0] + x][j+place[1] + y] = sign[i][j]
+                frame_to[i+x][j+y] = sign[i][j]
+    
+    #rectangle(frame_to,(y,x),(y+len(sign[0]),x+len(sign)),(0,255,0),2)
     return frame_to
 
 cap1 = VideoCapture(Video_Path[0])
 count = 0
-count_1 = 0
+count_1 = 2598
+
+sign1 = resize(img_for_training[2], sign_size1)
+sign2 = resize(img_for_training[2], sign_size2)
+sign3 = resize(img_for_training[2], sign_size3)
 
 while 1:
-    frame1 = cap1.read()[1]
-    frame2 = cap1.read()[1]
-    frame3 = cap1.read()[1]
-    
-    sign1 = resize(img_for_training[0], sign_size1)
-    sign2 = resize(img_for_training[0], sign_size2)
-    sign3 = resize(img_for_training[0], sign_size3)
-    
-    
-    if count % every_frame == 0:
-        x1, y1 = rand_chords(frame1, sign1)
-        x2, y2 = rand_chords(frame2, sign2)
-        x3, y3 = rand_chords(frame3, sign3)
+    try:
+        frame1 = cap1.read()[1]
+        frame2 = cap1.read()[1]
+        frame3 = cap1.read()[1]
         
+        if count % every_frame == 0:
+            
         
-        #frame_to_write = add_sign_without_background(frame, sign1, 0, 0)
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1), add_sign_without_background_to_image(frame1, sign1, x1, y1))
-        #print(f"index = {count_1+1}: and x value = {x1}")
-        #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1), frame_to_write)
-        
-        #frame_to_write2 = add_sign_without_background(frame, sign2, -70 , -60)
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+2), add_sign_without_background_to_image(frame2, sign2, x2 , y2))
-        #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+2), frame_to_write2)
-        
-        #frame_to_write3 = add_sign_without_background(frame, sign3, -80, 110)
-        imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+3), add_sign_without_background_to_image(frame3, sign3, x3, y3))
-        #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+3), frame_to_write3)
-        count_1 = count_1 + 3
-    count = count + 1
+            x1, y1 = rand_chords(frame1, sign1)
+            x2, y2 = rand_chords(frame2, sign2)
+            x3, y3 = rand_chords(frame3, sign3)
+            
+            
+            #frame_to_write = add_sign_without_background(frame, sign1, 0, 0)
+            imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1), add_sign_without_background_to_image(frame1, sign1, x1, y1))
+            #print(f"index = {count_1+1}: and x value = {x1}")
+            #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+1), frame_to_write)
+            
+            #frame_to_write2 = add_sign_without_background(frame, sign2, -70 , -60)
+            imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+2), add_sign_without_background_to_image(frame2, sign2, x2 , y2))
+            #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+2), frame_to_write2)
+            
+            #frame_to_write3 = add_sign_without_background(frame, sign3, -80, 110)
+            imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+3), add_sign_without_background_to_image(frame3, sign3, x3, y3))
+            #imwrite(Path_to_save_images + "/%#05d.jpg" % (count_1+3), frame_to_write3)
+            count_1 = count_1 + 3
+        count = count + 1
+    except KeyboardInterrupt:
+        print("Interupt")
+        break
     
